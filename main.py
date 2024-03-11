80b3d5f3ca51290dbe79f3652b6bc6a629136c19
dvYurchenko <yurchenko.dasha13@gmail.com>
2024-03-11 19:39:11 +0500

    Update main.py
    Новые изменения

--- main.py
+++ main.py
@@ -22,3 +22,42 @@ with open(file,'r') as file:
         for word in words:
             if word not in empty_set:
                 file_3.write(word + ' ')
+
+#Задание 2
+my_dict = {'Арвидас Сабонис' : 221,'Ральф Сэмпсон' : 224,'Яо Мин' : 229, 'Келвин Мерфи' : 175, 'Дэйна Баррос' : 178, 'Майкс Джордан' : 198, 'Коби Брайант' : 198}
+print(my_dict)
+while(True):
+    print('Задание 1. Добавить данные о баскетболисте:')
+    print('Задание 2. Удалить данные о баскетболисте:')
+    print('Задание 3. Найти данные о баскетболисте:')
+    print('Задание 4. Изменить данные о баскетболисте:')
+    print('0-выход.')
+    print('*'*11)
+    value=int(input('Введите номер задания: '))
+    if (value==1):
+        def dobavlenie(value1, value2):
+            my_dict2 = {value1 : value2}
+            my_dict.update(my_dict2)  # перезапись на новые значения
+            print(my_dict)
+        value1 = input('Введите имя баскетболиста: ')
+        value2 = input('Введите рост баскетболиста: ') 
+        dobavlenie(value1, value2)
+    if (value == 2):
+        def udalenie(value3):
+            del my_dict[value3]
+            print(my_dict)
+        value3 = input('Введите имя баскетболиста, которого нужно удалить: ')
+        udalenie(value3)
+    if (value == 3):
+        def poisk(value4):
+            print('Рост баскетболиста: ', my_dict.get(value4))
+        value4 = input('Введите имя баскетболиста: ')
+        poisk(value4)
+    if (value == 4):
+        def zamena(value5):
+            for key in my_dict:
+                if key == value5:
+                    my_dict[key] = input('Введите рост баскетболиста')
+
+
+        value5 = input('Введите имя баскетболиста: ')
