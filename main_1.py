9722ada78bc89d293af50e4f785f7d608aa1ed0a
dvYurchenko <yurchenko.dasha13@gmail.com>
2024-03-13 21:05:47 +0500

    Create main_1.py
    Новый документ

---
+++ main_1.py
@@ -0,0 +1,7 @@
+#from sys import path
+#path.append('..\\modules')
+from modules.modul_pr import alpfa, omega
+zero_list=[0 for i in range(10)]
+one_list=[1 for i in range(10)]
+print(alpfa(zero_list))
+print(omega(one_list))
